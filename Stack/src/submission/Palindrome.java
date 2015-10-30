b80c85f9448101a0b5161a414612493035849e71
McLean, Robert <rmclean@drew.edu>
2015-10-29 23:19:23 -0400

    Implemented Palindrome

--- Stack/src/submission/Palindrome.java
+++ Stack/src/submission/Palindrome.java
@@ -2,8 +2,22 @@ package submission;
 
 public class Palindrome {
 	public static boolean isPalindrome(String s) {
-		// TODO use a stack to determine if s is palindrome
-		return false;
+		if(s.length() == 0 || s.length() == 1)
+            return true; 
+        if(s.charAt(0) == s.charAt(s.length()-1))
+        /* check for first and last char of String:
+         * if they are same then do the same thing for a substring
+         * with first and last char removed. and carry on this
+         * until you string completes or condition fails
+         * Function calling itself: Recursion
+         */
+        return isPalindrome(s.substring(1, s.length()-1));
+
+        /* If program control reaches to this statement it means
+         * the String is not palindrome hence return false.
+         */
+        return false;
+		
 	}
 }
 //
