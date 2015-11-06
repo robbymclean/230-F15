22dd6bddd6ed9cb1784be448a6461bf31d2dd4fe
McLean, Robert <rmclean@drew.edu>
2015-11-06 01:05:37 -0500

    Implemented ArrayQueue

--- Queue/src/submission/ArrayQueue.java
+++ Queue/src/submission/ArrayQueue.java
@@ -1,40 +1,67 @@
 package submission;
-
+import java.util.NoSuchElementException;
 import adt.Queue;
 
 public class ArrayQueue<T> implements Queue<T> {
 	private T[] data;
 	private int head, tail;
 	
+
 	@Override
 	public void enqueue(T newEntry) {
 		// TODO Auto-generated method stub
+		tail = (tail + 1) % data.length;
+		data[tail] = newEntry;
+		
 		
 	}
 
 	@Override
 	public T dequeue() {
 		// TODO Auto-generated method stub
-		return null;
+		if(isEmpty())
+			throw new NoSuchElementException();
+		else
+		{
+			T front = data[head];
+			data[head] = null;
+			head = (head + 1) % data.length;
+			return front;
+		}
+		
 	}
 
 	@Override
 	public T peek() {
 		// TODO Auto-generated method stub
-		return null;
+		if(isEmpty())
+			throw new NoSuchElementException();
+		else
+			return data[head];
 	}
 
 	@Override
 	public boolean isEmpty() {
 		// TODO Auto-generated method stub
-		return false;
+		
+		return head == ((tail + 1)% data.length);
 	}
 
 	@Override
 	public void clear() {
 		// TODO Auto-generated method stub
-		
+		if(isEmpty()){
+			for(int i = head; i != tail; i = (i+1)%
+			data.length) {
+				data[i] = null;
+			}
+		data[tail] = null;
 	}
+		head = 0;
+		tail = data.length -1;
+	}
+	
+	
 	
 	public String toString() {
 		String s = data[head].toString();
@@ -43,5 +70,12 @@ public class ArrayQueue<T> implements Queue<T> {
 		s+= "\n";
 		return s;
 	}
-
+ public static void main(String[]args){
+	 Queue<String> anotherqueue = new ArrayQueue<>();
+	 anotherqueue.enqueue("What");
+	 anotherqueue.enqueue("It's Robby");
+	 anotherqueue.enqueue("My major is Comp Sci");
+	 String name = anotherqueue.dequeue();
+	 System.out.println(anotherqueue);
+ }
 }
