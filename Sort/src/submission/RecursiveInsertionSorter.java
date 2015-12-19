37d58178187b92e1e1bc246ca880e040a374ae8b
McLean, Robert <rmclean@drew.edu>
2015-12-18 20:04:32 -0500

    fixed recursive insertion sorter. passed all tests

--- Sort/src/submission/RecursiveInsertionSorter.java
+++ Sort/src/submission/RecursiveInsertionSorter.java
@@ -1,19 +1,28 @@
 package submission;
 
 public class RecursiveInsertionSorter {
-	public static int[] sort(int[] unsorted, int n) {
-		int i;
-		if(n < 1)
-			sort(unsorted,n -1);
-		else{
-			int k = unsorted[n];
-			i = n-1;
-			while( i>0 && unsorted[i] > k){
-				unsorted[i+1] = unsorted[i];
-				i = i-1;
-			}
-			unsorted[i+1] = k;
+
+	
+	public static int[] sort(int[] unsorted) {
+		return sort(unsorted, unsorted.length-1); // Don't touch
+	}
+
+	private static int[] sort(int[] unsorted, int endIndex) {
+		// TODO implement recursive insertion sort where
+		// endIndex is the index of the last unsorted element
+		if(endIndex == 0){
+			return unsorted;
 		}
-		return unsorted;
+		else{
+			sort(unsorted, endIndex - 1);
+			int result = unsorted[endIndex];
+			int i = endIndex - 1;
+			while( i>- 1 && result < unsorted[i]){
+				unsorted[i+ 1] = unsorted[i];
+				i--;
+			}
+			unsorted[i + 1] = result;
+			return unsorted;
+		}
 	}
 }
