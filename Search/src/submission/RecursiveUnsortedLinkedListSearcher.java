9b3f9aadff465690b0a6728d0cb6d4d0698701c6
McLean, Robert <rmclean@drew.edu>
2015-12-18 21:58:40 -0500

    fixed RecursiveUnsortedLinkedList and passed all tests

--- Search/src/submission/RecursiveUnsortedLinkedListSearcher.java
+++ Search/src/submission/RecursiveUnsortedLinkedListSearcher.java
@@ -1,17 +1,19 @@
 package submission;
 
 public class RecursiveUnsortedLinkedListSearcher {
+	 static boolean found;
 	public static boolean search(Node a, int key) {
 		// Returns true if the key is found or
-		// false if not
-		boolean found;
-		 if(a == null)
-			 found = false;
-		 else if(key.equals(a.num)){
-			 return true;
-		 }
-            
-		 else
-			 found = search(a.next , key);
+		
+		if(a.num() == key){
+			found = true;
+		}
+		else if(a.getNext()!= null){
+			search(a.getNext(), key);
+		}
+		else{
+			found = false;
+		}
+		return found;
 	}
 }
